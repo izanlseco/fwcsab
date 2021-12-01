2cea46d1734a23e67555f42d57974c7f3a2e6420
Izan Lopez Seco <IzanLS09@gmail.com>
2021-12-01 13:02:09 +0100

    [FWCSAB-06] Update test 	- Add assert for each entry in whenSummaryContainsMultipleMatchOrderedArrayIsReturned

--- src/test/java/team/service/TeamServiceTest.java
+++ src/test/java/team/service/TeamServiceTest.java
@@ -13,7 +13,11 @@ import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
 public class TeamServiceTest {
 
     private final static int FIRST_INDEX = 0;
-    private final static int LAST_INDEX = 5;
+    private final static int SECOND_INDEX = 1;
+    private final static int THIRD_INDEX = 2;
+    private final static int FOURTH_INDEX = 3;
+    private final static int FIFTH_INDEX = 4;
+    private final static int SIXTH_INDEX = 5;
     private final static String HOME_TEAM_NAME = "homeTeam.name";
     private final static String HOME_TEAM_SCORE = "homeTeam.score";
     private final static String AWAY_TEAM_NAME = "awayTeam.name";
@@ -76,7 +80,19 @@ public class TeamServiceTest {
         assertThat(response.get(FIRST_INDEX))
                 .extracting(HOME_TEAM_NAME, HOME_TEAM_SCORE, AWAY_TEAM_NAME, AWAY_TEAM_SCORE)
                 .contains("Brazil", 5, "Russia", 5);
-        assertThat(response.get(LAST_INDEX))
+        assertThat(response.get(SECOND_INDEX))
+                .extracting(HOME_TEAM_NAME, HOME_TEAM_SCORE, AWAY_TEAM_NAME, AWAY_TEAM_SCORE)
+                .contains("USA", 2, "Holland", 8);
+        assertThat(response.get(THIRD_INDEX))
+                .extracting(HOME_TEAM_NAME, HOME_TEAM_SCORE, AWAY_TEAM_NAME, AWAY_TEAM_SCORE)
+                .contains("Portugal", 3, "Ukraine", 5);
+        assertThat(response.get(FOURTH_INDEX))
+                .extracting(HOME_TEAM_NAME, HOME_TEAM_SCORE, AWAY_TEAM_NAME, AWAY_TEAM_SCORE)
+                .contains("Denmark", 5, "Italy", 3);
+        assertThat(response.get(FIFTH_INDEX))
+                .extracting(HOME_TEAM_NAME, HOME_TEAM_SCORE, AWAY_TEAM_NAME, AWAY_TEAM_SCORE)
+                .contains("Spain", 3, "Germany", 5);
+        assertThat(response.get(SIXTH_INDEX))
                 .extracting(HOME_TEAM_NAME, HOME_TEAM_SCORE, AWAY_TEAM_NAME, AWAY_TEAM_SCORE)
                 .contains("Switzerland", 2, "Romania", 2);
     }
